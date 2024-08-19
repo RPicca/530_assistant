afa1eddeb87768d3a9a14405b644ca080b508140
Romain PICCA <rpicca@forsk.com>
2024-08-19 15:38:51 +0200

    Calcul de K

--- 530_Assistant.py
+++ 530_Assistant.py
@@ -1,9 +1,10 @@
 import win32com.client
 import PySimpleGUI as sg
 import os
-import sys
+import openpyxl
+import numpy as np
 
-path = "D:\Documents\Atoll_Macros"
+path = "\\\sisyphe\\TestsStorage\\Tests_Atoll\\Traitements\\MW\\Feuilles de calculs SMath\\530 - Interruptions dues à la pluie et aux multi-trajets"
 
 
 def print_to_window(message):
@@ -28,13 +29,16 @@ def print_to_window(message):
 
 
 def Custom_report(dir_path):
+    dir_path = os.path.join(dir_path, "CR")
+    if not os.path.isdir(dir_path):
+        os.mkdir(dir_path)
     doc = Atoll.ActiveDocument
     links = doc.GetRootFolder(0).Item("Links").Item("Microwave Links")
     inputs = doc.GetCommandDefaults("MWLinksCustomExport")
     inputs.Set("EXPORTPATH", dir_path)
     inputs.Set(
         "TEMPLATEFILEPATH",
-        "D:\Documents\Temp\CR_530-18.txt")
+        os.path.join(dir_path, "Template_CR_530-18.txt"))
     inputs.Set("LINKSFOLDER", links)
     output = doc.InvokeCommand("MWLinksCustomExport", inputs)
 
@@ -66,17 +70,87 @@ def AtollMacro_write_smath_files():
         with open(os.path.join(dir_path, k + ".txt"), "w") as file:
             file.write(str(dico[k]))
     Custom_report(dir_path)
-    print(read_CR(link_name))
+    dico_CR = read_CR(os.path.join(dir_path, "CR", link_name))
+    for k in dico_CR:
+        with open(os.path.join(dir_path, k + ".txt"), "w") as file:
+            # Useful for min phase radio params
+            if dico_CR[k].split(" ")[0] == "1e-6":
+                string = dico_CR[k].split(" ")[1]
+            # Remove digit separator and Unit and other stuff...
+            else:
+                string = str(dico_CR[k]).split(" ")[0].replace(",", "").replace(
+                    "Yes", "1").replace("No", "0").replace("<Ignore>", "").replace("n/a", "0")
+            try:
+                float(string)
+            except:
+                if string == "":
+                    string = "0"
+                else:
+                    string = "\"" + string + "\""
+            file.write(string)
 
 
 def read_CR(link_name):
     dico = {}
     with open(link_name + ".txt", "r") as file:
-        for r in file.readlines:
-            dico[r.split[": "][0]] = r.split[": "][1]
+        for r in file.readlines():
+            dico[r.split(": ")[0]] = r.split(": ")[1].replace("\n", "")
     return dico
+
+
+def find_4_closest_MW_Calculated_Value(file, lat, lon):
+    table = open(file, 'r')
+    content = []
+    for l in table.readlines():
+        list = [float(i) for i in l.split(";")]
+        content.append(list)
+    table.close()
+    i = j = 1
+    while (content[0][j] - lon) * (content[0][j + 1] - lon) > 0:
+        j += 1
+    while (content[i][0] - lat) * (content[i + 1][0] - lat) > 0:
+        i += 1
+    lon_lat = [[content[0][j], content[0][j + 1]],
+               [content[i][0], content[i + 1][0]]]
+    values = [[content[i][j], content[i][j + 1]],
+              [content[i + 1][j], content[i + 1][j + 1]]]
+    return lon_lat, values
+
+
+def linear_interpolation(x, coord, values):
+    if coord[0] > coord[1]:
+        coord.reverse()
+        values.reverse()
+    slope = (values[1] - values[0]) / (coord[1] - coord[0])
+    return values[0] + slope * np.abs(x - coord[0])
+
+
+def bilinear_interpolation(lat, lon, lon_lat, values):
+    tmp = [linear_interpolation(lon, lon_lat[0], values[0]), linear_interpolation(lon, lon_lat[0], values[1])]
+    return linear_interpolation(lat, lon_lat[1], tmp)
+
+
+def AtollMacro_K():
+    ppt = Get_Selected_Link_Properties()
+    lon = (ppt["ABS_X_A"] + ppt["ABS_X_B"]) / 2
+    lat = (ppt["ABS_Y_A"] + ppt["ABS_Y_B"]) / 2
+    lon_lat, values = find_4_closest_MW_Calculated_Value(
+        "\\\sisyphe\\TestsStorage\\Tests_Atoll\\Traitements\MW\\Feuilles de calculs SMath\\530 - Interruptions dues à la pluie et aux multi-trajets\\530-18_calculated_values\\LogK_merged.csv",
+        lat, lon)
+    print("K = " + str(10**bilinear_interpolation(lat, lon, lon_lat, values)))
 
 
 def AtollMacro_Print_Selected_Link_Properties():
     print = print_to_window
     print(Get_Selected_Link_Properties())
+
+
+lon = -6.1806828726247485
+lat = 53.288487911977015
+lon_lat, values = find_4_closest_MW_Calculated_Value(
+    "\\\sisyphe\\TestsStorage\\Tests_Atoll\\Traitements\MW\\Feuilles de calculs SMath\\530 - Interruptions dues à la pluie et aux multi-trajets\\530-18_calculated_values\\LogK_merged.csv",
+    lat, lon)
+
+K = 10**bilinear_interpolation(lat, lon, lon_lat, values)
+
+print(K)
